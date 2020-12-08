34041881339e9cf090b5e5ca7797f2580e9ac7d7
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2020-12-08 16:06:35 -0300

    links for each location

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:third_flutter/models/location.dart';
+import 'models/location.dart';
+import 'location_detail.dart';
+import 'styles.dart';
 
 class LocationList extends StatelessWidget {
   final List<Location> locations;
 
-  LocationDetail(this.locations);
+  LocationList(this.locations);
 
-  @overide
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Locations', style: Styles.navBarTitle)),
-        body: null,
-    )
+      appBar: AppBar(title: Text('Locations', style: Styles.navBarTitle)),
+      body: ListView.builder(
+        itemCount: this.locations.length,
+        itemBuilder: _listViewItemBuilder,
+      ),
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var location = this.locations[index];
+    return ListTile(
+        contentPadding: EdgeInsets.all(10.0),
+        leading: _itemThumbnail(this.locations[index]),
+        title: _itemTitle(this.locations[index]),
+        onTap: () => _navigateToLocationDetail(context, this.locations[index]));
+  }
+
+  void _navigateToLocationDetail(BuildContext context, Location location) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LocationDetail(location),
+        ));
+  }
+
+  Widget _itemThumbnail(Location location) {
+    return Container(
+      constraints: BoxConstraints.tightFor(width: 100.0),
+      child: Image.network(location.url, fit: BoxFit.fitWidth),
+    );
+  }
+
+  Widget _itemTitle(Location location) {
+    return Text(location.name, style: Styles.textDefault);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:third_flutter/mocks/mock_location.dart';
-import 'models/location.dart';
-import 'location_detail.dart';
+import 'location_list.dart';
 import 'mocks/mock_location.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
+  final mockLocations = MockLocation.fetchAll();
 
-  return runApp(MaterialApp(home: LocationDetail(mockLocation)));
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
 }
